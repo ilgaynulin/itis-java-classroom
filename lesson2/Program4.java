75ada8b5551ef1a71afb7060b51d22ead192fc24
ilgaynulin <ilgaynulin@gmail.com>
2022-09-24 11:29:14 +0300

    добавил программу со switch

---
+++ lesson2/Program4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Program4 {
+	public static void main(String[] args) {
+		int dayOfWeek;
+
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("Введите день недели: ");
+		dayOfWeek = in.nextInt();
+
+		switch(dayOfWeek) {
+			case 1:
+				System.out.println("Понедельник");
+				break;
+			case 2:
+				System.out.println("Вторник");
+				break;
+			default:
+				System.out.println("Я не знаю что это за день!");
+				break;
+		}
+	}
+}
