45d235899e198fa584ba60f4b1d237e0b12e5a8f
ilgaynulin <ilgaynulin@gmail.com>
2022-10-01 10:45:00 +0300

    добавил прогу с массивом

---
+++ lesson3/TemperatureCollector.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TemperatureCollector {
+	public static void main(String [] args) {
+		Scanner in = new Scanner(System.in);
+		double measurements[];
+		
+		System.out.print("Введите сколько значений вы хотите добавить: ");
+		int arrayDimmention = in.nextInt();
+
+		measurements = new double[arrayDimmention];
+
+		System.out.print("Вводите значения температуры!");
+
+		for(int i = 0; i < measurements.length; i++) {
+			System.out.print("Введите элемент номер " + i + ":");
+			measurements[i] = in.nextDouble();
+		}
+
+		System.out.println("Выводим на консоль записанные значения");
+		// Выводим на консоль
+		for(int i = 0; i < measurements.length; i++) {
+			System.out.println(measurements[i]);
+		}
+	}
+}
