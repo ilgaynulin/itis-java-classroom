f051a6f5f62002fb9968d505f4a941c07bd7126f
Ильяс Гайнулин Р <ilgaynulin@gmail.com>
2022-11-10 14:01:06 +0300

    добавил остальное

--- lesson8/RegularExprEmail.java
+++ lesson8/RegularExprEmail.java
@@ -1,19 +1,23 @@
 import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class RegularExprEmail {
 	public static void main(String[] args) {
-		String text = "my email @ is ilgaynulin@gmail.com   I don't have reserve mail";
+		String text = "my email @ is ilgaynulin@gmail.com   my reserve mail  ilyas@gmail.com ";
 		String regEx = "\\s[a-z]+@[a-z\\.]+\\s";
 
 		Pattern p = Pattern.compile(regEx);
 		Matcher m = p.matcher(text);
 
-		int count = 0;
+		int count = 1;
 		while(m.find()) {
 			System.out.println("Номер найденного слова: " + count);
+			System.out.println("text: " + text);
+			System.out.println("regEx: " + regEx);
 			int foundBeginIndex = m.start();
 			int foundEndIndex = m.end();
-			System.out.println("foundBeginIndex = " + foundBeginIndex + "foundEndIndex = " + foundEndIndex);
+			System.out.println("foundBeginIndex = " + foundBeginIndex + " foundEndIndex = " + foundEndIndex);
+			System.out.println(text.substring(foundBeginIndex, foundEndIndex));
 			count++;
 		}
 	}
