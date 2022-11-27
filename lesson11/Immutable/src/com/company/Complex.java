04961907cd3bdb08738fc08b6c92966ff9acda9f
ilgaynulin <ilgaynulin@gmail.com>
2022-11-27 10:22:21 +0300

    добавил по неизменяемым классам

--- lesson11/Immutable/src/com/company/Complex.java
+++ lesson11/Immutable/src/com/company/Complex.java
@@ -29,7 +29,30 @@ public class Complex {
         return sumResult;
     }
 
+    public boolean equals(Object object) {
+        if(this == object) {
+            return true;
+        }
+
+        if(this.getClass() != object.getClass()) {
+            return false;
+        }
+
+        if(this.real == ((Complex) object).real && this.imaginary == ((Complex) object).imaginary) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     // minus
+    public Complex minus(Complex c) {
+        return new Complex(this.real - c.real, this.imaginary - c.imaginary);
+    }
 
     // multiply
+
+    public Complex multiply(Complex c) {
+        return new Complex(this.real * c.real - this.imaginary * c.imaginary, this.real * c.imaginary + this.imaginary * c.real);
+    }
 }
--- lesson11/Immutable/src/com/company/Main.java
+++ lesson11/Immutable/src/com/company/Main.java
@@ -5,8 +5,15 @@ public class Main {
     public static void main(String[] args) {
         Complex c1 = new Complex(1.0, 2.0);
         Complex c2 = new Complex(2.0, 3.0);
+        Complex c3 = new Complex(1.0, 2.0);
 
-        Complex sum = c1.sum(c2);
-        System.out.println(sum);
+//        Complex sum = c1.sum(c2);
+//        System.out.println(sum);
+        System.out.println("c1 equals c2 " + c1.equals(c2));
+        System.out.println("c1 equals c3 " + c1.equals(c3));
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c1.multiply(c2));
     }
 }
