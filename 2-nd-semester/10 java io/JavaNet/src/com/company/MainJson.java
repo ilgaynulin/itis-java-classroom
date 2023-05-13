e8d294613b8ba573481b234478d23d315022b9a7
ilgaynulin <ilgaynulin@gmail.com>
2023-05-13 12:25:27 +0300

    добавил оставшееся

--- 2-nd-semester/10 java io/JavaNet/src/com/company/MainJson.java
+++ 2-nd-semester/10 java io/JavaNet/src/com/company/MainJson.java
@@ -1,24 +1,16 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class MainJson {
 
     public static void main(String[] args) throws IOException {
         URL url = new URL("https://api.coindesk.com/v1/bpi/currentprice.json");
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        StringBuilder resultLine = new StringBuilder();
-        String line;
-        while((line = br.readLine()) != null) {
-            resultLine.append(line);
-        }
-        BitcoinPriceParser.getUSDPriceFromText(resultLine.toString());
+        FileOutputStream fos = new FileOutputStream("output.txt");
+        InputStream is = conn.getInputStream();
+        fos.write(is.readAllBytes());
     }
 }
