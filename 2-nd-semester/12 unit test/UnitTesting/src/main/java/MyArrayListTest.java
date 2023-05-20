d2bfa7b43671dcf16419e74b78388ce457669cd2
ilgaynulin <ilgaynulin@gmail.com>
2023-05-20 15:09:13 +0300

    добавил остальное

--- 2-nd-semester/12 unit test/UnitTesting/src/main/java/MyArrayListTest.java
+++ 2-nd-semester/12 unit test/UnitTesting/src/main/java/MyArrayListTest.java
@@ -1,6 +1,40 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class MyArrayListTest {
+    MyArrayList<String> strings;
 
+    @Before
+    public void init() {
+        strings = new MyArrayList<>();
+    }
+
+    @Test
     public void test_whenPutElement_thenAssertElementInList() {
+        strings.put("Element");
+        Assert.assertEquals("Element", strings.get(0));
+    }
 
+    @Test
+    public void anotherTest_whenPutElementInEmpty_thenAssertElementNotInSecondIndex() {
+        strings.put("Joke");
+        Assert.assertNotEquals("Joke", strings.get(1));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void test_whenGetNegativeIndex_thenThrowException() {
+        strings.get(-1);
+    }
+
+    @Test
+    public void test_whenDeleteZeroIndex_ThenSizeDecreased() {
+        strings.put("Element1");
+        strings.put("Element2");
+        strings.delete(0);
+        int expectedSize = 1;
+        int actualSize = strings.getSize();
+
+        Assert.assertEquals(expectedSize, actualSize);
     }
 }
